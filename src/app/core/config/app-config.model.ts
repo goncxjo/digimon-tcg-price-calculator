3c20eaee987c2aee52b3214592e400f79e5817a7
Moreira, Gonzalo <gonzalo-adrian.moreira@atos.net>
2023-09-26 22:25:50 -0300

    actualización app config nuevas variables

--- src/app/core/config/app-config.model.ts
+++ src/app/core/config/app-config.model.ts
@@ -4,6 +4,10 @@ export interface AppConfig {
 	CARD_TRADER_API_JWT_TOKEN: string;
 	CARD_TRADER_API_GAME_ID: number;
 	CARD_TRADER_API_CATEGORY_ID: number;
+	TCG_PLAYER_API_SEARCH_ENDPOINT: string;
+	TCG_PLAYER_API_PRODUCT_ENDPOINT: string;
+	TCG_PLAYER_API_PRICE_ENDPOINT: string;
+	TCG_PLAYER_API_IMAGE_ENDPOINT: string;
 	DOLAR_API_BASE_URL: string;
 	appVersion: string;
 	production: boolean;
