09dfe5e090812599ece3d3a544b4d174b749294d
Moreira, Gonzalo <gonzalo-adrian.moreira@atos.net>
2023-09-26 22:24:36 -0300

    refactor model carta

--- src/app/backend/models/card.ts
+++ src/app/backend/models/card.ts
@@ -1,11 +1,9 @@
-import * as uuid from 'uuid';
-import { BlueprintCardTrader } from "./card-trader";
- 
- export class Card {
+export interface Card {
 	id: string;		
 	name: string;
 	fullName: string;
 	expansion_id: number;		
+	image_small_url: string;
 	image_url: string;
 	card_trader_id: number | null;
 	card_market_id: number | null;
@@ -14,40 +12,6 @@ import { BlueprintCardTrader } from "./card-trader";
 	rarity_name: string;
 	collector_number: string;
 	expansion_name: string;
-	price: number = 0;
-	format: string = "USD";
-
-	constructor(blueprint: any) {
-		this.id = uuid.v4();
-		this.name = blueprint.name;
-		this.expansion_id = blueprint.expansion_id;
-		this.rarity_code = blueprint.fixed_properties.digimon_rarity;
-		this.collector_number = blueprint.fixed_properties.collector_number;
-		this.image_url = blueprint.image_url;
-		this.card_trader_id = blueprint.id;
-		this.card_market_id = (blueprint.card_market_id || null) as number | null;
-		this.tcg_player_id = (blueprint.tcg_player_id || null) as number | null;
-		this.expansion_name = blueprint.expansion_name;
-		this.rarity_name = this.getRarity();
-		this.fullName = `${this.name} (${this.collector_number})`;
-	}
-
-	getRarity(): string {
-		switch (this.rarity_code) {
-			case "C":
-				return "Common";
-			case "U":
-				return "Uncommon";
-			case "R":
-				return "Rare";
-			case "SR":
-				return "Supe Rare";
-			case "SEC":
-				return "Secret";
-			case "P":
-				return "Promo";
-			default:
-				return this.rarity_code;
-		}
-	}
+	price: number;
+	format: string;
 }
