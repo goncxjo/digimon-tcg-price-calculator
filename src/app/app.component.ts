02cd526e6a013a20ad2c671d0b073affd8a4560f
Moreira, Gonzalo <gonzalo-adrian.moreira@atos.net>
2023-09-27 23:18:40 -0300

    gestión de títulos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter, map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'digimon-tcg-price-calculator';
+export class AppComponent implements OnInit {
+  title: string = 'Cargando...';
+
+  constructor(
+    private titleService: Title,
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+  ) { }
+
+  ngOnInit(): void {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+      map(() => {
+        let title: string | null = null;
+        let showPageTitle = true;
+
+        let child = this.activatedRoute.firstChild;
+        while (child?.firstChild) {
+          child = child.firstChild;
+        }
+        if (child?.snapshot.data['title']) {
+          title = child.snapshot.data['title'];
+        }
+        if (child?.snapshot.data['pageTitleHidden']) {
+          showPageTitle = !child.snapshot.data['pageTitleHidden'];
+        }
+        return { title, showPageTitle };
+      }),
+    ).subscribe((cfg) => this.setTitle(cfg));
+  }
+
+  private setTitle(cfg: any) {
+    this.title = cfg.showPageTitle ? cfg.title : '';
+    this.titleService.setTitle('Price Calculator' + (cfg.title ? ' - ' + cfg.title : ''));
+  }
 }
