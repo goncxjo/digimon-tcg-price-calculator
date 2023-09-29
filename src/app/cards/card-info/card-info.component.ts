8d0068412904eac75f7188f0db0ace0fd649c0f5
Moreira, Gonzalo <gonzalo-adrian.moreira@atos.net>
2023-09-29 13:58:58 -0300

    se agrega seleccionador de precios

--- src/app/cards/card-info/card-info.component.ts
+++ src/app/cards/card-info/card-info.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { Card, Dolar } from 'src/app/backend/models';
+import { ProductPriceTcgPlayer } from 'src/app/backend/models/tcg-player';
 
 @Component({
   selector: 'app-card-info',
@@ -9,16 +10,29 @@ import { Card, Dolar } from 'src/app/backend/models';
 export class CardInfoComponent implements OnInit, OnDestroy {
   @Input() data!: Card;
   @Input() dolar!: Dolar;
+  priceSelected: string = "tcg_player_normal";
     
   ngOnInit(): void {
+  }
+  
+  getPrecioCarta() {
+    switch (this.priceSelected) {
+      case 'tcg_player_normal':
+      case 'tcg_player_foil':
+        const printingType = this.priceSelected === 'tcg_player_normal' ? 'Normal' : 'Foil';
+        const price = this.getTcgPlayerPrice(printingType);
+        return this.data.price.currency_value = price * this.dolar.venta;
+      default:
+        return this.data.price.currency_value;
+    }
+  }
+
+  getTcgPlayerPrice(printingType: string) {
+    return this.data.tcg_player_price.find(p => p.printingType === printingType)?.listedMedianPrice || 0;
   }
 
   cartaTienePrecio() {
     return this.getPrecioCarta() !== 0;
-  }
-
-  getPrecioCarta() {
-    return (this.data.price * this.dolar.venta);
   }
 
   getPrecioCartaFixed() {
@@ -27,6 +41,10 @@ export class CardInfoComponent implements OnInit, OnDestroy {
 
   getFechaActualizacionDolar() {
     return (new Date(this.dolar.fechaActualizacion)).toLocaleString('es-AR')
+  }
+
+  onPriceSelected(priceSelected: string) {
+    this.priceSelected = priceSelected;
   }
 
   ngOnDestroy(): void {
