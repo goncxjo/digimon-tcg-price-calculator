42df3511f1047c209b6b79bd67fa2eb2dc92fe88
goncxjo <gonzalo.moreira@live.com.ar>
2023-10-07 23:30:33 -0300

    se ordenan meta tags

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -64,8 +64,10 @@ export class AppComponent implements OnInit {
 
   updateMetaTags() {
     const url = `${window.location.protocol}//${window.location.hostname}`;
-    this.meta.updateTag({property: 'og:image', content: `${url}/assets/og-image-logo.png`});
     this.meta.updateTag({property: 'og:url', content: `${url}`})
+    this.meta.updateTag({property: 'og:image', content: `${url}/assets/og-image-logo.png`});
+    this.meta.updateTag({property: 'twitter:url', content: `${url}`})
+    this.meta.updateTag({property: 'twitter:image', content: `${url}/assets/og-image-logo.png`});
   }
 
   private setTitle(cfg: any) {
