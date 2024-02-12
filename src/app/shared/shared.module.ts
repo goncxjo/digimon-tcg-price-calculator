68e91e2bde90240856a0e92283c4a1a6d653ee5e
Moreira, Gonzalo <gonzalo-adrian.moreira@atos.net>
2024-02-12 11:50:23 -0300

    se agregan selectores si-no y currency

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -8,6 +8,8 @@ import { fas } from '@fortawesome/free-solid-svg-icons';
 import { far } from '@fortawesome/free-regular-svg-icons';
 import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 import { ReversePipe } from './reverse.pipe';
+import { YesNoSelectComponent } from './yes-no-select/yes-no-select.component';
+import { CurrencySelectComponent } from './currency-select/currency-select.component';
 
 const MODULES = [
   CommonModule,
@@ -21,7 +23,9 @@ const PRIVATE_COMPONENTS: any = [
 ];
 
 const PUBLIC_COMPONENTS: any = [
-  ReversePipe
+  ReversePipe,
+  YesNoSelectComponent,
+  CurrencySelectComponent
 ];
 
 @NgModule({
