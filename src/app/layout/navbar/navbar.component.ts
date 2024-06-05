1795f849b0ef976cdd4d8b948d358711cd8485b0
Gonzalo Moreira <gonzalo.moreira@live.com.ar>
2024-06-05 10:58:32 -0300

    wip: navbar y footer

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -1,14 +1,32 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { CommonModule } from '@angular/common';
+import { CommonModule, CurrencyPipe } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { IconDefinition, faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, FontAwesomeModule, CurrencyPipe],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
   public isLoading: boolean = false;
+  lightIcon = faSun;
+  darkIcon = faMoon;
+  themeIcon: IconDefinition = this.lightIcon;
+  dolar = {
+    compra: 1200,
+    venta: 1200
+  }
+
+  toggleTheme() {
+    if (this.themeIcon == this.darkIcon) {
+      this.themeIcon = this.lightIcon;
+    }
+    else {
+      this.themeIcon = this.darkIcon;
+    }
+  }
 }
