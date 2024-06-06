18806056c53bcc1ffa1106071c2690617ef9b15a
goncxjo <gonzalo.moreira@live.com.ar>
2024-06-06 01:00:31 -0300

    llamada al modal card-search

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faCalculator, faGear, faHeart, faHome, faSearch } from '@fortawesome/free-solid-svg-icons';
+import { NgbModal, NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
+import { CardSearchModalComponent } from '../../components/cards/card-search-modal/card-search-modal.component';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [FontAwesomeModule, RouterLink],
+  imports: [FontAwesomeModule, RouterLink, NgbModalModule],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
@@ -17,17 +19,26 @@ export class FooterComponent {
   favIcon = faHeart;
   settingsIcon = faGear;
 
-  constructor(
-    private router: Router,
-  ) {
-    
-  }
-
   items: any[] = [
     { icon: this.homeIcon, command: () => { this.router.navigate(['/'])} },
     { icon: this.calcIcon, command: () => { this.router.navigate(['/old'])} },
-    { icon: this.searchIcon, command: () => {} },
+    { icon: this.searchIcon, command: () => { this.openCardSearchModal() } },
     { icon: this.favIcon, command: () => {} },
     { icon: this.settingsIcon, command: () => {} },
-  ]
+  ];
+  
+  constructor(
+    private router: Router,
+    private modalService: NgbModal
+  ) { }
+
+
+  openCardSearchModal() {
+    const modalRef = this.modalService.open(CardSearchModalComponent, {
+      size: 'lg',
+      centered: true,
+      scrollable: true,
+      modalDialogClass: 'card-search-modal-height' 
+    });
+  }
 }
