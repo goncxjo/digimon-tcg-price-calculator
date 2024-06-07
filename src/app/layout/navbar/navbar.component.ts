65ebef42af72573d2d9b3c9f67d238385313f227
goncxjo <gonzalo.moreira@live.com.ar>
2024-06-07 01:17:06 -0300

    wip: dark mode

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { CommonModule, CurrencyPipe } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { IconDefinition, faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
+import { IconDefinition, faCircleHalfStroke, faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
+import { AppThemeService } from '../../core/services/app-theme.service';
+import _ from 'lodash';
 
 @Component({
   selector: 'app-navbar',
@@ -11,22 +13,46 @@ import { IconDefinition, faMoon, faSun } from '@fortawesome/free-solid-svg-icons
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
   public isLoading: boolean = false;
-  lightIcon = faSun;
-  darkIcon = faMoon;
-  themeIcon: IconDefinition = this.lightIcon;
-  dolar = {
-    compra: 1200,
-    venta: 1200
+  readonly defaultTheme = {
+    name: 'light',
+    icon: faSun
+  };
+  theme = this.defaultTheme;
+
+  availableThemes = {
+    light: this.defaultTheme,
+    dark: { name: 'dark', icon: faMoon}
+  }
+
+  // dolar = {
+  //   compra: 1200,
+  //   venta: 1200
+  // }
+
+  appThemeService: AppThemeService = inject(AppThemeService);
+
+  ngOnInit(): void {
+    this.theme = this.getTheme();
   }
 
   toggleTheme() {
-    if (this.themeIcon == this.darkIcon) {
-      this.themeIcon = this.lightIcon;
+    if (this.isDarkTheme(this.theme)) {
+      this.theme = this.availableThemes.light;
+      }
+      else {
+      this.theme = this.availableThemes.dark;
     }
-    else {
-      this.themeIcon = this.darkIcon;
-    }
+    this.appThemeService.update(this.theme.name)
+  }
+
+  getTheme() {
+    const theme = _.find(this.availableThemes, ['name', this.appThemeService.theme()]);
+    return theme ?? this.defaultTheme;
+  }
+
+  isDarkTheme(theme: any) {
+    return this.availableThemes.dark == theme;
   }
 }
