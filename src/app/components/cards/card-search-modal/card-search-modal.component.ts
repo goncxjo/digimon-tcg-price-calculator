ca4f85ce8e6690df95bd412e5cd6fa86d95a54c8
goncxjo <gonzalo.moreira@live.com.ar>
2024-06-07 00:00:41 -0300

    se separan los filtros en un componente nuevo

--- src/app/components/cards/card-search-modal/card-search-modal.component.ts
+++ src/app/components/cards/card-search-modal/card-search-modal.component.ts
@@ -1,55 +1,39 @@
 import { Component, inject } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faCircleExclamation, faSearch, faSliders, faTriangleExclamation } from '@fortawesome/free-solid-svg-icons';
+import { faSearch, faSliders, faTriangleExclamation } from '@fortawesome/free-solid-svg-icons';
 import { NgbActiveModal, NgbHighlight } from '@ng-bootstrap/ng-bootstrap';
 import { Card, FiltersTcgPlayerQuery, TcgPlayerService } from '../../../backend';
 import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { BehaviorSubject, Observable, catchError, debounceTime, distinctUntilChanged, of, switchMap, tap } from 'rxjs';
 import _ from 'lodash';
 import { AsyncPipe } from '@angular/common';
-import { trigger, transition, style, animate } from '@angular/animations';
-import { RarityMultiSelectComponent } from '../../../shared/rarity-multiselect/rarity-multiselect.component';
-import { ColorMultiSelectComponent } from '../../../shared/color-multiselect/color-multiselect.component';
-import { ExpansionSelectComponent } from '../../../shared/expansion-select/expansion-select.component';
-import { CategorySelectComponent } from '../../../shared/category-select/category-select.component';
+import { CardSearchFiltersComponent } from '../card-search-filters/card-search-filters.component';
 
 @Component({
   selector: 'app-card-search-modal',
   standalone: true,
-  imports: [ReactiveFormsModule, FontAwesomeModule, NgbHighlight, AsyncPipe, RarityMultiSelectComponent, ColorMultiSelectComponent, CategorySelectComponent, ExpansionSelectComponent],
+  imports: [ReactiveFormsModule, FontAwesomeModule, NgbHighlight, AsyncPipe, CardSearchFiltersComponent],
   templateUrl: './card-search-modal.component.html',
   styleUrl: './card-search-modal.component.scss',
-  animations: [
-    trigger('myInsertRemoveTrigger', [
-      transition(':enter', [
-        style({ opacity: 0 }),
-        animate('200ms', style({ opacity: 1 })),
-      ]),
-      transition(':leave', [
-        animate('200ms', style({ opacity: 0 }))
-      ])
-    ]),
-  ]
 })
 export class CardSearchModalComponent {
   searchIcon = faSearch;
   warningIcon = faTriangleExclamation;
-  infoIcon = faCircleExclamation;
   filetersIcon = faSliders;
 
 	activeModal = inject(NgbActiveModal);
-
-  form = this.buildForm();
+  
   searchCard$ = new BehaviorSubject<string>('');
   cardSearchTextInput = new FormControl();
   searching = false;
-
-  mostrarBusquedaAvanzada = false;
-  esPreRelease = false;
-
   get term() {
     return this.cardSearchTextInput.value;
   }
+  
+  selectedCards: Card[] = [];
+
+  form = this.buildForm();
+  mostrarBusquedaAvanzada = false;  
 
   constructor(
     private tcgPlayerService: TcgPlayerService,
@@ -76,13 +60,16 @@ export class CardSearchModalComponent {
     tap(() => (this.searching = false)),
   );
 
-  ngOnInit(): void {
-  }  
-  
   private buildForm(): FormGroup {
-    return this.formBuilder.group({
-      isPreRelease: [''],
-    });
+    return this.formBuilder.group({});
+  }
+
+  toggleBusquedaAvanzada() {
+    this.mostrarBusquedaAvanzada = !this.mostrarBusquedaAvanzada;
+    if(!this.mostrarBusquedaAvanzada) {
+      this.form.reset();
+      this.form.controls['isPreRelease'].setValue(false)
+    }
   }
 
   mapFilters(): FiltersTcgPlayerQuery {
@@ -92,23 +79,9 @@ export class CardSearchModalComponent {
       categories: values?.category ? [values?.category] : [],
       colors: values?.colors,
       rarities: values?.rarities,
-      isPreRelease: this.esPreRelease
+      isPreRelease: values?.esPreRelease
     }
   }
-
-
-  toggleBusquedaAvanzada() {
-    this.mostrarBusquedaAvanzada = !this.mostrarBusquedaAvanzada;
-    if(!this.mostrarBusquedaAvanzada) {
-      this.form.reset();
-      this.esPreRelease = false;
-      this.form.controls['isPreRelease'].setValue(this.esPreRelease)
-    }
-  }
-
-  onIsPreReleaseChanged($event: any) {
-    this.esPreRelease = $event.target.checked;
-  }    
 
 }
 
