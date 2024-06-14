ff724a23ba50e2acc31f2ef98e03e9086aecf2c3
goncxjo <gonzalo.moreira@live.com.ar>
2024-06-14 00:19:11 -0300

    se mueve barra dolar a navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './layout/navbar/navbar.component';
-import { DolarComponent } from './layout/dolar/dolar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, DolarComponent],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
