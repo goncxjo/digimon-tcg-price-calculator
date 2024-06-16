4a65ee5bb9cfa5af299c457a7c7def4c22d2e839
goncxjo <gonzalo.moreira@live.com.ar>
2024-06-16 01:32:19 -0300

    se agrega contenido en home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { LogoComponent } from '../../layout/logo/logo.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [LogoComponent, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
 }
